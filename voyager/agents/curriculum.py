f5ab9965e871700b9a5d1ced352401dac75b9ef1
ElianBelot <elian.belot@mail.mcgill.ca>
2023-07-04 22:54:36 -0700

    curriculum boilerplate setup

--- voyager/agents/curriculum.py
+++ voyager/agents/curriculum.py
@@ -4,13 +4,23 @@
 
 # ==========[ CURRICULUM AGENT ]==========
 class CurriculumAgent:
+    """
+    This is about creating system and user messages that will be run to propose the next task.
+    This also keeps track of progress and tasks.
+
+    [POC] Run "propose_next_task" successfully (executer in a notebook, we just care about messages).
+    [TASKS]
+    - Create system to keep track of messages (output of propose_next_task is a list of messages?)
+    - Keep track of progress
+    """
+
     # ==========[ GOAL ]==========
     def set_goal(self, goal):
-        # Implement this method.
+        # This sets the goal in the system prompt
         pass
 
     def get_goal_progress(self, completed_tasks, failed_tasks):
-        # Implement this method.
+        #
         pass
 
     # ==========[ PROPOSE NEXT TASK ]==========
