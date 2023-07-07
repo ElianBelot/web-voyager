914d1d97a6817431bbc1f127b69ff9aa2e18a2d2
ElianBelot <elian.belot@mail.mcgill.ca>
2023-07-06 20:00:48 -0700

    added code execution

--- skill_library/trial1/code/get_wikipedia_article_summary.py
+++ skill_library/trial1/code/get_wikipedia_article_summary.py
@@ -1,10 +1,14 @@
-def get_wikipedia_article_summary(driver, article_title: str):
+def get_wikipedia_article_summary(driver, article_title: str = "Machine Learning"):
     # Navigate to the article
     url = f"https://en.wikipedia.org/wiki/{article_title}"
+    print("Navigating to URL:", url)
     driver.get(url)
 
     # Find the first paragraph. Wikipedia typically structures the summary with 'p' tag after the first 'table' tag.
-    summary_element = driver.find_element(By.XPATH, "//table/following-sibling::p")
+    summary_element = driver.find_element(selenium.webdriver.common.by.By.XPATH, "//table/following-sibling::p")
+    print("Found summary element:", summary_element)
 
     # Extract and return the text
-    return summary_element.text
+    summary_text = summary_element.text
+    print("Extracted summary text:", summary_text)
+    return summary_text
--- voyager/environment/environment.py
+++ voyager/environment/environment.py
@@ -1,23 +1,57 @@
-# ==========[ IMPORTS ]==========
-...
+import contextlib
+import io
+import traceback
+import types
+from typing import Any, Dict
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 # ==========[ ENVIRONMENT ]==========
 class Environment:
-    def __init__(self):
-        pass
+    def __init__(self, headless: bool = False):
+        # Set Chrome options
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--no-sandbox")
+        if headless:
+            self.chrome_options.add_argument("--headless")
+
+        # Set driver
+        self.driver = webdriver.Chrome(options=self.chrome_options)
 
     def reset(self):
-        # Resets the environment to its initial state.
-        pass
+        # Closes the driver and start a new one
+        self.driver.quit()
+        self.driver = webdriver.Chrome(options=self.chrome_options)
 
-    def step(self, code):
-        # 1. Execute the provided code as action, optionally with a semantic layer.
-        # 2. Capture any execution errors.
-        # 3. Capture code output.
-        # 4. Generate feedback based on the execution.
-
+    def step(self, code, arguments: Dict[str, Any] = {}):
+        # Generate a namespace for the code to be executed in
         code_output = None
         execution_errors = None
+        log = []
+        namespace = {"selenium": selenium}
 
-        return code_output, execution_errors
+        # Redirect standard output to a string stream
+        string_io = io.StringIO()
+        with contextlib.redirect_stdout(string_io):
+            # Execute the code
+            try:
+                exec(code, namespace)
+            except Exception as e:
+                execution_errors = traceback.format_exc()
+
+            # Extract the function defined in the code, which should be the only top-level item.
+            for value in namespace.values():
+                if isinstance(value, types.FunctionType):
+                    # Invoke the function, passing the driver as the first argument.
+                    try:
+                        code_output = value(self.driver, **arguments)
+                    except Exception as e:
+                        execution_errors = traceback.format_exc()
+
+        # Add the printed text to the log
+        log.extend(string_io.getvalue().splitlines())
+
+        return {"output": code_output, "errors": execution_errors, "log": log}
